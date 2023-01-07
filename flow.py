45c0e3512d4d005f6bd809638be88d9fe478d684
Martin <chmhou@sina.com>
2023-01-07 22:50:57 +0800

    Add files via upload
    增加py文件，抓取图片，重写README.md 文件

---
+++ flow.py
@@ -0,0 +1,55 @@
+#/usr/bin/python3
+
+from bs4 import BeautifulSoup
+import urllib.request, pdb, sys, random
+
+SF2_PIC = [
+    'https://www.vgmaps.com/Atlas/Genesis/ShiningForceII-World.png',
+]
+
+def DoHttp(url=''):
+    with urllib.request.urlopen(url) as f:
+        ret = f.read()
+        return ret.decode('utf-8')
+    return None
+
+def SelectPic(url):
+    c = DoHttp(url)
+    if c == None:
+        sys.exit()
+    soup = BeautifulSoup(c, "html.parser")
+    soup.prettify()
+    elements_div = soup.td.findAll('div')
+    for div in elements_div:
+        if div.table and div.table.a and div.table.a.attrs and div.table.a.attrs['name']:
+            print(div.table.a.attrs['name'])
+            if div.table.a.attrs['name'] != 'ShiningForceII':
+                continue
+
+            tr_list = div.table.findAll('tr')
+            if len(tr_list) > 2:
+                tr_list = tr_list[2:]
+            else:
+                continue
+            pic_url = tr_list[random.randrange(len(tr_list)-1)]
+            print(pic_url.a.attrs['href'])
+            return pic_url.a.attrs['href']
+
+def Update(name,url):
+    f = open('README.md','r+')
+    old = f.read()
+    f.seek()
+    f.write('<img decoding="async" src="{0}" width="50%" alt="what??">'.format(url))
+    f.write(old)
+
+
+def run():
+    #name, url = constants.PickRandomURL()
+    base_url = 'https://www.vgmaps.com/Atlas/Genesis/'
+    pic = SelectPic(base_url + 'index.htm')
+    if pic:
+        pic = base_url + pic
+    Update(pic)
+
+if __name__ == '__main__':
+    run()
